e3a529e8dfc6ea80a078c6659ed0c8611893bc06
Lorenzo Maturano <maturanolorenzo@gmail.com>
2021-09-24 16:10:23 -0300

    feat: sending bitcoin transaction with op_return

--- routes/api.js
+++ routes/api.js
@@ -9,6 +9,7 @@ router.get('/api', (req, res, next) => {
 router.post('/api', (req, res, next) => {
     if (req.body.address) {
         res.json(req.body.address);
+        // Make transaction
     } else {
         res.json({
             'error': 'Something went wrong'
@@ -22,26 +23,77 @@ const headers = {
 
 const USER = 'user';
 const PASS = 'pass';
-let dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getblockcount","params":[]}`;
-let options = {
-    url: `http://${USER}:${PASS}@127.0.0.1:18332/`,
-    method: "POST",
-    headers: headers,
-    body: dataString
-};
+let data = {
+    jsonrpc: "1.0",
+    id: "curltext",
+    method: "getblockcount",
+    params: []
+}
+const url = `http://${USER}:${PASS}@127.0.0.1:18332/`;
 
 const getBlockCount = async () => {
     try {
-        const response = await axios.post(options.url, options.body);
+        const response = await axios.post(url, data);
         return response.data.result
     } catch (error) {
         console.log(error);
     }
 }
 
+
+const createTransaction = async () => {
+    try {
+        data.method = "createrawtransaction"
+        data.params = [
+            [{
+                "txid": "2a406a96a393fb5d808d30aa1f954b7b8ecff4fb0053a038e6c03d36c2043de3","vout": 1
+            }],
+            {
+                "tb1ql7w62elx9ucw4pj5lgw4l028hmuw80sndtntxt": 0.000001,
+                "data": "48656c6c6f20576f726c64",
+                "tb1qkyg8p5g0k893284xuweulhvnmttu6qqgkwaajt": 0.000817
+            }
+        ]
+        const response = await axios.post(url, data);
+        return response.data.result;
+    } catch (error) {
+        console.log(error.response.data);
+    }
+}
+
+const signTransaction = async (hexcode) => {
+    try {
+        data.method = "signrawtransactionwithwallet";
+        data.params = [hexcode]
+        const response = await axios.post(url, data);
+        return response.data.result;
+    } catch (error) {
+        console.log(error.response.data);
+    }
+}
+
+const broadcastTransaction = async (signedTx) => {
+    try {
+        data.method = "sendrawtransaction"
+        data.params = [signedTx]
+        const response = await axios.post(url, data);
+        return response.data.result;
+    } catch (error) {
+        console.log(error.response.data);
+    }
+}
+
 router.get('/getblockcount', async (req, res) => {
     const response = await getBlockCount();
     res.json({blockcount: response})
+})
+
+router.get('/createtransaction', async (req, res) => {
+    const hexcode = await createTransaction();
+    const signedTx = await signTransaction(hexcode);
+    const response = await broadcastTransaction(signedTx.hex)
+ 
+    res.json({response: response})
 })
 
 
