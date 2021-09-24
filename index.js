747ff5b0a0aae937e630ba8dc142fc540e1c1598
Lorenzo Maturano <maturanolorenzo@gmail.com>
2021-09-24 09:46:33 -0300

    hello world route

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello World")
+const express = require('express');
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+app.listen(port, () => {console.log(`Server listening @ http://localhost:${port}`)})
