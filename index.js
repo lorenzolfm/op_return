94dd97c181c43e4c81ada38edc50187d15b1d931
Lorenzo Maturano <maturanolorenzo@gmail.com>
2021-09-24 09:50:26 -0300

    basic routing

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express()
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     res.send('Hello World');
 })
+
+app.post('/api', (req, res) => {
+    res.send('Got a POST request')
+})
+
 
 app.listen(port, () => {console.log(`Server listening @ http://localhost:${port}`)})
