549cd7a08f54e0f55940cb712ca7dcb9a371f693
Lorenzo Maturano <maturanolorenzo@gmail.com>
2021-09-24 14:13:40 -0300

    WIP: talking to bitcoind

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const axios = require('axios');
 const router = express.Router();
 
 router.get('/api', (req, res, next) => {
-    res.json("GET request @ /api");
+    res.json({msg: "GET request @ /api"});
 })
 
 router.post('/api', (req, res, next) => {
@@ -10,9 +11,38 @@ router.post('/api', (req, res, next) => {
         res.json(req.body.address);
     } else {
         res.json({
-            "error": "Empty address"
+            'error': 'Something went wrong'
         })
     }
 })
+
+const headers = {
+    "content-type": "text/plain"
+};
+
+const USER = 'user';
+const PASS = 'pass';
+let dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getblockcount","params":[]}`;
+let options = {
+    url: `http://${USER}:${PASS}@127.0.0.1:18332/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+};
+
+const getBlockCount = async () => {
+    try {
+        const response = await axios.post(options.url, options.body);
+        return response.data.result
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+router.get('/getblockcount', async (req, res) => {
+    const response = await getBlockCount();
+    res.json({blockcount: response})
+})
+
 
 module.exports = router;
